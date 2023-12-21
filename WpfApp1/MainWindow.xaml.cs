16bf6f538f7b28fc169724f239a5fd044b7264e8
Rintsel <rintsler.druse@gmail.com>
2023-12-22 00:23:37 +0300

    Прогрмма с логикой распознавания артикула

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,69 +1,59 @@
 ﻿using System;
 using System.Data.SQLite;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows;
-
 
 namespace WpfApp1
 { 
     public partial class MainWindow : Window
     {
-
         public MainWindow()
         {
-            using (SQLiteConnection connection = new SQLiteConnection(DatabaseManager.ConnectionString))
-            {
-                connection.Open();
-                using (SQLiteCommand countCommand = new SQLiteCommand("SELECT COUNT(*) FROM Steklopakety", connection))
-                {
-                    int rowCount = Convert.ToInt32(countCommand.ExecuteScalar());
-
-                    if (rowCount == 0)
-                    {
-                        DatabaseManager.CreateDatabase();
-                        DatabaseManager.InsertData("6 SG HD Silver Grey 32 ЗАК/16/6 м1/16/6 ЗАК", 2, 50, 18);
-                        DatabaseManager.InsertData("4/16/4", 1, 24, 8);
-                        DatabaseManager.InsertData("4 TOP-N/14/6_м1", 1, 24, 10);
-                        DatabaseManager.InsertData("4 ПлА2/16/4м1", 1, 24, 8);
-                        DatabaseManager.InsertData("4 TOP-N/14ar/4 м1/14ar/4_StClBr", 2, 40, 12);
-                        DatabaseManager.InsertData("10 м1/22/8 м1", 1, 40, 18);
-                        InitializeComponent();
-                    }
-                    else
-                    {
-                        // База данных не пуста, выполните необходимые действия (возможно, обработка этой ситуации)
-                        MessageBox.Show("База данных не пуста, не добавлены новые данные.");
-                    }
-                }
-            }
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            using (SQLiteConnection connection = new SQLiteConnection(DatabaseManager.ConnectionString))
+            OutputText.Text = "";
+            char s = ' ';
+            int i = 0, d = 0, tsp = 0, ts = 0;
+            // Переменная для хранения найденного числа
+            string foundNumber = string.Empty;
+
+            // Проверка наличия символа разделителя
+            bool Slash = artikul.Text.Contains('/');
+            bool Underscore = artikul.Text.Contains('_');
+            bool BS = artikul.Text.Contains(' ');
+            bool flag= false;
+
+            if (Slash) {  s = '/'; }
+            else if (Underscore) { s = '_'; }
+            else if (BS) { s = ' '; }
+
+            // Разделить строку по символу '/'
+            string[] parts = artikul.Text.Split(s);
+
+            // Найти первое число в каждой части
+            foreach (var part in parts)
             {
-                connection.Open();
+                i++;
+                Regex regex = new Regex(@"\d+"); // Поиск числа
+                Match match = regex.Match(part);
 
-                string selectDataSql = @"
-                SELECT Artikul, Kamernost, TolshinaSP, TolshinaStekla
-                FROM Steklopakety
-                WHERE Artikul = @Artikul;
-            ";
-
-                using (SQLiteCommand command = new SQLiteCommand(selectDataSql, connection))
+                if (match.Success)
                 {
-                    command.Parameters.AddWithValue("@Artikul", artikul.Text);
-
-                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    if (!(i % 2 == 0))
                     {
-                        while (reader.Read())
-                        {
-                            OutputText.Text = $"Камерность: {reader["Kamernost"]}, \n" +
-                                $"Толщина стеклопакета: {reader["TolshinaSP"]}, \n" +
-                                $"Толщина стекла: {reader["TolshinaStekla"]}";
-                        }
+                        ts += int.Parse(match.Value);
+                        d++;
                     }
+                    tsp += int.Parse(match.Value);
                 }
             }
+            OutputText.Text = (d-1).ToString() + " - Камерный \n";
+            OutputText.Text += "Толщина СП = " + tsp +'\n';
+            OutputText.Text += "Толщина стекла = " + ts + '\n';
         }
     }
 }
